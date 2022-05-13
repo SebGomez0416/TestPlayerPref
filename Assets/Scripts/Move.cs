005e35c9130013d869f9f78eced962fc1e4b6563
juan sebastian gomez <sebbastian.gc@gmail.com>
2022-05-13 14:10:45 -0300

    Add move player

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 movement;
+    [SerializeField] private float speed;
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        Vector3 pos = rb.position;
+        pos.z = PlayerPrefs.GetFloat("position",0f);
+        rb.position = pos;
+    }
+
+    private void Update()
+    {
+        movement.z = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement*(speed*Time.deltaTime));
+    }
+}
